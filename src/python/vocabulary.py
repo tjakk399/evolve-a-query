ef02adb43dcffe3edb2b1661c97444922cd0dc32
david <david@carbi>
2021-08-27 13:48:36 +0200

    Added reST-style docstrings to vocabulary module

--- src/python/vocabulary.py
+++ src/python/vocabulary.py
@@ -8,7 +8,16 @@ import random
 
 
 class Vocabulary():
+    """Class for managing vocabulary.
+
+    :param words: List of words from some text,
+        defaults to [].
+    :type words: List[str], optional
+    """
+
     def __init__(self, words: List(str) = []):
+        """Constructor method
+        """
         self.words = defaultdict(int)
         self.add_words(words)
 
@@ -25,28 +34,64 @@ class Vocabulary():
                 )
 
     @staticmethod
-    def _tokenized(text: str):
+    def _tokenized(text: str) -> List[str]:
+        """Returns tokenized string.
+
+        :param text: String
+        :type text: str
+
+        :return: List of words
+        :rtype: List[str]
+        """
         return text.split()
 
-    def add_words(self, words):
+    def add_words(self, words: List[str]) -> None:
+        """Adds word to dictionary counter.
+
+        :param words: List of words
+        :type words: List[str]
+        """
         for word in words:
             self.words[word] += 1
 
-    def add_words_from(self, text: str):
-        self.add_words(Vocabulary._tokenized(text))
+    def add_words_from(self, text: str) -> None:
+        """Adds words from text to dictionary counter.
+
+        :param text: Text string
+        :type text: str
+        """
+        self.add_words(
+                Vocabulary._tokenized(text)
+                )
 
     def sample(self,
             n: int = 1,
             without: List[str] = []
             ) -> List[str]:
+        """Returns random sample words from vocabulary.
+
+        :param n: Number of sample words to be returned
+        :type n: int
+
+        :param without: Blacklist of words to not include in returned sample
+        :type without: List[str]
+
+        :return: Sample words
+        :rtype: List[str]
+        """
         return random.sample(
                 [
                     word
-                    for word in list(self.words.keys())
+                    for word in self.wordlist()
                     if word not in without
                     ],
                 n
                 )
 
-    def wordlist(self):
+    def wordlist(self) -> List[str]:
+        """Returns list of unique words from vocabulary.
+
+        :return: List of words
+        :rtype: List[str]
+        """
         return list(self.words.keys())
