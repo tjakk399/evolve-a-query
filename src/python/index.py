02cc5dfc725466048171624e4232b8c53874ac35
david <david@carbi>
2021-08-27 13:19:29 +0200

    Added reST-style docstrings to index module

--- src/python/index.py
+++ src/python/index.py
@@ -10,12 +10,26 @@ from vocabulary import Vocabulary
 
 
 class Index():
+    """Class for managing access Elasticsearch index.
+
+    :param name: Index name
+    :type name: str
+
+    :param host Elasticsearch host URL
+    :type host: str
+
+    :param port Elasticsearch host port
+    :type port: int
+    """
+
     def __init__(
             self,
             name: str,
             host: str = "localhost",
             port: int = 9200,
             ):
+        """Constructor method
+        """
         self.name = name
 
         self.es = elasticsearch.Elasticsearch(
@@ -32,7 +46,13 @@ class Index():
 
         self.ensure_index()
 
-    def ensure_index(self):
+    def ensure_index(self) -> None:
+        """Creates or recreates index.
+
+        :rtype: None
+
+        :raises Exception: if index creation failed
+        """
         try:
             self.es.indices.delete(
                 index = self.name,
@@ -60,18 +80,33 @@ class Index():
             logging.ERROR(str(e))
             exit(1)
 
-    def _bulk_data_generator(self, texts: List[str]):
+    def _bulk_data_generator(self, texts: List[str]) -> Dict:
+        """Generator for document indexing actions.
+
+        :param texts: List of documents
+        :type texts: List[str]
+
+        :return: Action
+        :rtype: Dict
+        """
         for text in texts:
             yield {
                     '_op_type': "index",
                     '_index': self.name,
-                    # '_id': self.id,
                     '_source': {
                         'full_text': text,
                         },
                     }
 
-    def add(self, text: str):
+    def add(self, text: str) -> Dict:
+        """Adds single document to index and adds its words to vocabulary.
+
+        :param text: Document text
+        :type text: str
+
+        :return: Elasticsearch response
+        :rtype: Dict
+        """
         self.vocabulary.add_words_from(text)
 
         return self.es.index(
@@ -82,30 +117,58 @@ class Index():
                     },
                 )
 
-    def add_bulk(self, texts = List[str]):
+    def add_bulk(self, texts: List[str]) -> Dict:
+        """Adds many documents to index and adds their words to vocabulary.
+
+        :param text: Document text
+        :type text: str
+
+        :return: Elasticsearch response
+        :rtype: Dict
+        """
         for text in texts:
             self.vocabulary.add_words_from(text)
 
-        return bulk(
+        return elasticsearch.helpers.bulk(
                 client = self.es,
                 actions = self._bulk_data_generator(texts),
                 refresh = "wait_for",
                 )
 
+    def get(self, id: int) -> Dict:
+        """Returns document from Index by ID.
 
-    def get(self, id: int):
+        :param id: ID of document in index
+        :type id: int
+
+        :return: Elasticsearch response
+        :rtype: Dict
+        """
         return self.es.get(
                 index = self.name,
                 id = id,
                 )
 
-    def search(self, query: Query):
+    def search(self, query: Query) -> Dict:
+        """Returns query response from index.
+
+        :param query: Query
+        :type query: :class:`Query`
+
+        :return: Elasticsearch response
+        :rtype: Dict
+        """
         return self.es.search(
                 index = self.name,
                 body = query.body
                 )
 
-    def random_document(self):
+    def random_document(self) -> Dict:
+        """Returns random document from index.
+
+        :return: Elasticsearch response
+        :rtype: Dict
+        """
         return self.es.search(
                 index = self.name,
                 body = {
@@ -121,11 +184,28 @@ class Index():
                     }
                 )
 
-    def explain(self, query: Query, id: int):
-        """Explanation for scoring"""
-        return self.es.explain(
-                index = self.name,
-                id = id,
-                body = query.body,
-                )
+    def explain(self, query: Query, id: int) -> Dict:
+        """Returns Elasticsearch explanation for match between query and document by ID.
+
+        :param query: Query
+        :type query: :class:`Query`
+
+        :param id: ID of document in index
+        :type id: int
+
+        :return: Elasticsearch response
+        :rtype: Dict
+
+        :raises Exception: if ID does not exist in index
+        """
+        try:
+            return self.es.explain(
+                    index = self.name,
+                    id = id,
+                    body = query.body,
+                    )
+
+        except Exception as e:
+            logging.ERROR(str(e))
+            exit(1)
 
