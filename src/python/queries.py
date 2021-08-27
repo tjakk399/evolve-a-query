f67b6ce08736610eee05d008df1f27a2054705ac
david <david@carbi>
2021-08-27 12:31:10 +0200

    Added reST-style docstrings to queries module

--- src/python/queries.py
+++ src/python/queries.py
@@ -24,10 +24,22 @@ class RecombinationMode(AutoNameEnum):
 
 
 class Queries(Population):
+    """Class for managing population of queries.
+
+    :param words: List of words as strings
+    :type words: List[str]
+
+    :param queries: List of :class:`Query` objects to initialize population,
+    defaults to empty list
+    :type queries: List[Query]
+    """
+
     def __init__(self,
-            words: list[str],
+            words: List[str],
             queries: List[Query] = [],
             ):
+        """Constructor method
+        """
         super().__init__(queries)
 
         # Just a different view with better name
@@ -35,14 +47,29 @@ class Queries(Population):
 
         self._words = words
 
-    def size(self):
+    def size(self) -> int:
+        """Returns number of queries.
+
+        :return: Number of queries
+        :rtype: int
+        """
         return len(self.queries)
 
     @sorter(key=lambda x: x.fitness, reverse=True)
     def sorted_queries(self) -> List[Query]:
+        """Returns queries sorted by fitness.
+
+        :return: List of sorted :class:`Query` objects
+        :rtype: List[Query]
+        """
         return self.queries
 
-    def average_score(self):
+    def average_score(self) -> int:
+        """Returns average score of queries.
+
+        :return: Average score of queries
+        :rtype: int
+        """
         if len(self.queries) == 0:
             return 0.0
         else:
@@ -54,8 +81,13 @@ class Queries(Population):
                     ) / len(self.queries)
 
     def recombine(self, mode: RecombinationMode=RecombinationMode.CLONE) -> None:
-        """
-        Recombine individuals and add their offspring to the population.
+        """Recombines queries according to given mode to produce offspring.
+        Adds offspring to populaton of queries.
+
+        :param mode: Strategy for producing offspring from queries, defaults to `RecombinationMode.CLONE`
+        :type mode: Member of enum :class:`RecombinationMode`, optional
+
+        :rtype: None
         """
 
         if mode == RecombinationMode.CLONE:
@@ -72,15 +104,22 @@ class Queries(Population):
     @staticmethod
     def _without_lowest(
             l: List,
-            key: Callable[[Any], float] = lambda x: x,
+            key: Callable = lambda x: x,
             ) -> List:
-        """
-        Return sublist without lowest elements,
-        as defined per key callable.
-
+        """Returns sublist without lowest elements, as defined per key callable.
         Note that in a list of equally valued elements,
         all elements are lowest per definition,
         so this would return an empty list.
+
+        :param l: List
+        :type l: List
+
+        :param key: Callable function that returns the value to be sorted on,
+            defaults to identity function
+        :type key: Callable
+
+        :return: Elements of ``l`` except for lowest elements
+        :rtype: List
         """
         l_sorted = sorted(
                 l,
@@ -92,7 +131,11 @@ class Queries(Population):
                 if key(e) > key(l_sorted[0])
                 ]
 
-    def mutate(self):
+    def mutate(self) -> None:
+        """Applies mutations across population of queries.
+
+        :rtype: None
+        """
         new_queries = []
 
         for query in self.queries:
@@ -102,14 +145,27 @@ class Queries(Population):
 
         self.queries = new_queries
 
-    def select(self):
+    def select(self) -> None:
+        """Reduces population of queries to fittest members.
+
+        :rtype: None
+        """
         self.queries = Queries._without_lowest(
                 self.queries,
                 key = lambda x: x.fitness
                 )
 
-    def random_purge(self, k: int=1):
-        """Remove k random members."""
+    def random_purge(self, k: int = 1) -> None:
+        """Removes random members from population.
+
+        :param k: Number of queries to be removed,
+            defaults to 1
+        :type k: int
+
+        :rtype: None
+
+        :raises ValueError:
+        """
         logging.debug("Removing random members from population")
 
         # assert k <= self.size()
@@ -136,6 +192,10 @@ class Queries(Population):
             raise e
 
     def remove_duplicates(self):
+        """Removes duplicate querys from population.
+
+        :rtype: None
+        """
         new_queries = []
         seen_queries = {}
 
